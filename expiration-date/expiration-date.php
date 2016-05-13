7297b207fed1c4e4e5646c61895f736dee1d98e0
Harrison <hmcguire13@gmail.com>
2016-05-13 10:52:17 -0700

    deleted meta check and added jquery calendar

--- expiration-date/expiration-date.php
+++ expiration-date/expiration-date.php
@@ -7,6 +7,12 @@ Author: Harrison McGuire
 Version: 1.0
  */
 
+function tutsplus_load_jquery_datepicker() {    
+    wp_enqueue_script( 'jquery-ui-datepicker' );
+    wp_enqueue_style( 'jquery-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
+}
+add_action( 'admin_enqueue_scripts', 'tutsplus_load_jquery_datepicker' );
+
 function tutsplus_add_expiry_date_metabox() {
     add_meta_box( 
         'tutsplus_expiry_date_metabox', 
@@ -21,10 +27,8 @@ add_action( 'add_meta_boxes', 'tutsplus_add_expiry_date_metabox' );
 
 
 function tutsplus_expiry_date_metabox_callback( $post ) { ?>
-
-	<?php wp_nonce_field( 'tutsplus_expiry_date_metabox_nonce', 'tutsplus_nonce' ); ?>
      
-    <form action="" method="post">
+ <form action="" method="post">
          
         <?php        
         //retrieve metadata value if it exists
@@ -33,19 +37,22 @@ function tutsplus_expiry_date_metabox_callback( $post ) { ?>
          
         <label for "tutsplus_expiry_date"><?php __('Expiry Date', 'tutsplus' ); ?></label>
                  
-        <input type="text" class="MyDate" name="tutsplus_expiry_date" value=<?php echo esc_attr( $tutsplus_expiry_date ); ?> / >            
+        <input type="text" class="MyDate" name="tutsplus_expiry_date" value=<?php echo esc_attr( $tutsplus_expiry_date ); ?> / >     
+
+        <script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('.MyDate').datepicker({
+            dateFormat : 'dd-mm-yy'
+        });
+    });
+    
+</script>       
      
     </form>
      
 <?php }
 
 function tutsplus_save_expiry_date_meta( $post_id ) {
-
-	if( !isset( $_POST['tutsplus_nonce'] ) ||
-    !wp_verify_nonce( $_POST['tutsplus_nonce'],
-    'tutsplus_expiry_date_metabox_nonce'
-    ) ) 
-    return;
      
     // Check if the current user has permission to edit the post. */
     if ( !current_user_can( 'edit_post', $post->ID ) )
